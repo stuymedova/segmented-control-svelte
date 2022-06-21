2d2c8d76e3792b79ffdf064cd6d3d4fbc28f2deb
Sofya Tuymedova <stuymedova@gmail.com>
2022-06-21 22:54:33 +0300

    chore: revert segment classname, and minor internal changes

--- src/lib/Segment.svelte
+++ src/lib/Segment.svelte
@@ -8,11 +8,11 @@
   let length = 0
   let offset = 0
 
-  const ctx = getContext('SegmentedControl')
-  const orientation = ctx.orientation
-  const index = ctx.setIndex()
-  const focusedSegmentIndex = ctx.focusedSegmentIndex
-  const selectedSegmentIndex = ctx.selectedSegmentIndex
+  const context = getContext('SegmentedControl')
+  const orientation = context.orientation
+  const index = context.setIndex()
+  const focusedSegmentIndex = context.focusedSegmentIndex
+  const selectedSegmentIndex = context.selectedSegmentIndex
   
   $: isFocused = $focusedSegmentIndex === index
   $: if (isFocused) { ref?.focus() }
@@ -27,14 +27,14 @@
       offset = Math.round(ref.offsetTop) 
     }
     
-    ctx.addSegment({ index, isDisabled, length, offset })
+    context.addSegment({ index, isDisabled, length, offset })
   })
 </script>
 
 
 <button 
   bind:this={ref}
-  class='segment'
+  class='segmented-control-item'
   type='button'
   role='tab'
   aria-selected={isSelected}
@@ -44,7 +44,7 @@
   on:click
   on:click|preventDefault={() => { 
     if (index !== $selectedSegmentIndex && !isDisabled) {
-      ctx.setSelected(index)
+      context.setSelected(index)
     }
   }}
   on:mouseover
@@ -57,11 +57,11 @@
   on:keydown='{({ key }) => {
     if (orientation === 'horizontal' && key === 'ArrowRight' || 
         orientation === 'vertical' && key === 'ArrowDown') {
-      ctx.setSelected(index + 1)
+      context.setSelected(index + 1)
     } else if (
         orientation === 'horizontal' && key === 'ArrowLeft' || 
         orientation === 'vertical' && key === 'ArrowUp') {
-      ctx.setSelected(index - 1)
+      context.setSelected(index - 1)
     }
   }}'
 >
